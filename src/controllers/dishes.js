e9251e7e9b4704b0c626ba1e86bfd8786332d548
Pratik <pagarw40@asu.edu>
2024-03-29 17:06:24 -0700

    Added code to push multiple dishes

--- src/controllers/dishes.js
+++ src/controllers/dishes.js
@@ -11,13 +11,18 @@ exports.getDishes = async (req, res) => {
 };
 
 exports.postDishes = async (req, res) => {
-  const { name, category, price} = req.body;
-  const newDish = new Dish({ name, category, price });
-  await newDish.save()
-  .then(function(){
-    res.json(newDish); // Response should send the dishes retrieved from the database
-  })
-  .catch(error => {
+  try {
+    const dishes = req.body; // Assuming req.body contains an array of dishes
+
+    const savedDishes = await Promise.all(dishes.map(async dishData => {
+      const { name, category, description, price, image } = dishData;
+      const parsedPrice = parseInt(price.replace(/\$/g, ''), 10); // Convert price string to integer
+      const newDish = new Dish({ name, category, description, price: parsedPrice, image });
+      return await newDish.save();
+    }));
+
+    res.json(savedDishes); // Response sends the dishes saved to the database
+  } catch (error) {
     res.status(500).json({ error: error.message });
-  });
+  }
 };
